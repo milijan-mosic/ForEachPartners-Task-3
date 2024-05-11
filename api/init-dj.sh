7ed8a5ff9f1a570653800a772433e82d3c71ebe8
milijan-mosic <milijan-mosic@protonmail.com>
2024-05-11 19:09:30 +0200

    Made basic default settings for Django

--- api/init-dj.sh
+++ api/init-dj.sh
@@ -5,6 +5,6 @@ set -e
 
 
 
-sudo docker-compose exec api python manage.py makemigrations
-sudo docker-compose exec api python manage.py migrate
-sudo docker-compose exec api python manage.py createsuperuser
+sudo docker-compose -f docker-compose.local.yaml exec api python manage.py makemigrations
+sudo docker-compose -f docker-compose.local.yaml exec api python manage.py migrate
+sudo docker-compose -f docker-compose.local.yaml exec api python manage.py createsuperuser
